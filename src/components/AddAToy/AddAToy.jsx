6f7fa0199bcfc0c33c08f41a20c889307ec0ae58
manik1241 <thmanik1241@gmail.com>
2023-05-19 19:36:58 +0600

    AddAToy.jsx added

--- src/components/AddAToy/AddAToy.jsx
+++ src/components/AddAToy/AddAToy.jsx
@@ -1,9 +1,89 @@
-
+import { useForm } from "react-hook-form";
+import './AddAToy.css'
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
 
 const AddAToy = () => {
+    const {user}=useContext(AuthContext)
+
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
+
+
     return (
-        <div>
-           <h2>This feature comming soon ....</h2> 
+        <div className=" ">
+            <div className="grid  md:grid-cols-2  sm:grid-cols-1 md:ms-96">
+                <div className="addAtoy ">
+                    <form className="form-formate" onSubmit={handleSubmit(onSubmit)}>
+
+                        <input
+                            className="text-input mt-2 py-3 px-14 px- me-4"
+                            {...register("sellerName")}
+                            placeholder="Seller Name"
+                            defaultValue={user.displayName}
+                        />
+                        <input
+                            className="text-input mt-2 py-3 px-14 px- me-4"
+                            {...register("email")}
+                            placeholder="Email address"
+                            defaultValue={user.email}
+                        />
+                        <input
+                            className="text-input mt-2 py-3 px-14 px- me-4"
+                            {...register("toyName")}
+                            placeholder="Toy Name"
+                            defaultValue=""
+                        />
+                        <input
+                            className="text-input mt-2 py-3 px-14 px- me-4"
+                            {...register("image")}
+                            placeholder=" Toy Image Link"
+                            defaultValue=""
+                        />
+                        <input
+                            className="text-input mt-2 py-3 px-14 px- me-4"
+                            {...register("price")}
+                            placeholder="Price"
+                            defaultValue=""
+                        />
+                        <input
+                            className="text-input mt-2 py-3 px-14 px- me-4"
+                            {...register("ratting")}
+                            placeholder="Product Ratting"
+                            defaultValue=""
+                        />
+                        <select className="md:me-16 py-3 px-20  mt-2" {...register("subCategory")}>
+                            <option disabled selected>Select Category</option>
+                            <option value="sport_car">sport-car</option>
+                            <option value="truck">truck</option>
+                            <option value="polic_car">polic-car</option>
+                        </select>
+                        <input
+                            className="text-input mt-2 py-3 px-14 px- md:me-4"
+                            {...register("available_quantity")}
+                            placeholder="Available Quantity"
+                            defaultValue=""
+                        />
+                        <textarea
+                            className="text-input mt-2 py-3 px-16 px- md:me-4"
+                            {...register("description")}
+                            placeholder="Product Description"
+                            defaultValue=""
+                        />
+
+
+                        {/* <input {...register("exampleRequired", { required: true })} /> */}
+
+                        {errors.exampleRequired && <span>This field is required</span>}
+
+
+
+                        <br></br><input className="btn submit-btn my-2 ms-1 w-72" type="submit" />
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/AllToy/AllToy.jsx
+++ src/components/AllToy/AllToy.jsx
@@ -3,7 +3,35 @@
 const AllToy = () => {
     return (
         <div>
-            <h3>all toy comming</h3>
+            <div className="overflow-x-auto">
+  <table className="table table-zebra w-full">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>SellerName</th>
+        <th>Toy Name</th>
+        <th>Sub-Category</th>
+        <th>Price</th>
+        <th>Quanity</th>
+        <th>Details</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+        <td>Blue</td>
+        <td>Blue</td>
+        <td><button className="btn">View Details</button></td>
+      </tr>
+     
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
