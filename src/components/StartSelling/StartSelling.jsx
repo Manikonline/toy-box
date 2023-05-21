ceed54b152c48fb56b36a579c65e8553db6542fa
manik1241 <thmanik1241@gmail.com>
2023-05-21 12:54:20 +0600

    Tabs.jsx task done

--- src/components/StartSelling/StartSelling.jsx
+++ src/components/StartSelling/StartSelling.jsx
@@ -1,15 +1,16 @@
 
+import { Link } from 'react-router-dom';
 import UseTitle from '../../Hooks/UseTitle';
 import './StartSelling.css'
 const StartSelling = () => {
     UseTitle('StartSelling')
     return (
         <div className="my-8">
-            <div className="flex mx-10  py-12 all-data justify-between px-8">
+            <div className="grid md:grid-cols-2 sm:grid-cols-1 mx-10  py-12 all-data justify-between text-center px-8">
                 <div>
-                    <h3 className='md:text-3xl font-bold md:ms-28 grid md:grid-cols-1 sm:grid-cols-2'>What are You Waiting for? <br></br> Start Selling with Toy Box !</h3>
+                    <h3 className='md:text-3xl font-bold  grid md:grid-cols-1 sm:grid-cols-2'>What are You Waiting for? <br></br> Start Selling with Toy Box !</h3>
                 </div>
-                 <div><button className="btn btn-style mt-4">GET START !!!</button>
+                 <div><button className="btn btn-style md:ms-10  mt-4"><Link to='/addatoy'>GET START !!!</Link></button>
                  </div>
             </div>
         </div>
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,62 +1,108 @@
 import { useEffect, useState } from 'react';
 import './Tabs.css'
+import { Link } from 'react-router-dom';
 
 
 const Tabs = () => {
     const [toggle, setToggle] = useState(1)
-    const [allProducts, setAllProducts]=useState([])
-    const [categoryData, setCategoryData]=useState([])
-    const handleToggle = (id ,subCategory) => {
-        setToggle(id );
+    const [allProducts, setAllProducts] = useState([])
+    const [categoryData, setCategoryData] = useState('sport_car')
+    const handleToggle = (id, subCategory) => {
+        setToggle(id);
         setCategoryData(subCategory)
         console.log(subCategory)
-      
+
     }
     console.log(categoryData)
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         fetch(`http://localhost:5000/subProducts?subCategory=${categoryData}`)
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data)
-            setAllProducts(data)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setAllProducts(data)
 
-        })
+            })
 
-    },[categoryData])
+    }, [categoryData])
 
-    
 
-   
+
+
     return (
-        <div className='md:mb-96 sm:mb-40'>
+       <div className='' >
+         <div className=''>
             <div className='py-2 my-3'>
-                <h1 className='text-center text-3xl font-bold'>Shop by category</h1>
+                <h1 className='text-center text-2xl font-bold'>Shop by category</h1>
             </div>
-           
-            <div className="text-center tab grid md:grid-cols-1 sm:grid-cols-1 ">
-                <div className='text-center m-auto'>
-                    <ul className="flex ">
-                        <li className="mx-3 px-2 active" onClick={() => handleToggle(1,'sport_car')}>Sport-car</li>
-                        <li className="mx-3 px-2" onClick={() => handleToggle(2,'truck')}>Truck</li>
-                        <li className="mx-3 px-2" onClick={() => handleToggle(3,'polic_car')}>Police-car</li>
-                    </ul>
+            <div className='flex items-center justify-center md:flex-row flex-col'>
+                   
+                        <button className="btn btn-sm btn-edit md:mx-2 sm:mx-1" onClick={() => handleToggle(1, 'sport_car')}>Sport-car</button>
+                        <button className="btn btn-sm btn-edit md:mx-2 sm:mx-1" onClick={() => handleToggle(2, 'truck')}>Truck</button>
+                        <button className="btn btn-sm btn-edit md:mx-2 sm:mx-1" onClick={() => handleToggle(3, 'polic_car')}>Police-car</button>
+                   
                 </div>
-                <div>
+        
+               
+                <div className='sm:mb-10'>
                     <div className={toggle === 1 ? "show-content" : "content"}>
-                        <div className='grid md:grid-cols-2 sm:grid-cols-1 my-20'>
-                           
-                          
+                        <div className='grid md:grid-cols-2 sm:grid-cols-1 w-[90%] mx-auto '>
+                            {
+                                allProducts?.slice(0 - 2).map(product => <><div><div className="card md:w-96 w-full bg-base-100 shadow-xl my-5  ">
+                                <figure className="px-10 pt-10">
+                                    <img src={product?.image} alt="Shoes" className="rounded-xl card-imagee" />
+                                </figure>
+                                <div className="card-body  ">
+                                    <h2 className="card-title text-start">{product?.toyName}</h2>
+                                    <p className='text-start'>Price: {product?.price}tk</p>
+                                    <div className="card-actions text-start">
+                                        <p>Rating: {product?.ratting}</p> <button className='view-details-btn'><Link to={`productDetails/${product?._id}`}>View Detais</Link></button>
+                                    </div>
+                                </div>
+                            </div></div></>)
+                            }
+
                         </div>
 
                     </div>
                     <div className={toggle === 2 ? "show-content" : "content"}>
-                        <h3>Truck</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic necessitatibus similique tempore exercitationem unde nulla nemo assumenda maiores alias neque.</p>
+                        <div className='grid md:grid-cols-2  sm:grid-cols-1  w-[90%] mx-auto  '>
+                            {
+                                allProducts?.slice(0 - 2).map(product => <><div className="card flex md:w-96 w-full bg-base-100 shadow-xl my-5 ">
+                                    <figure className="px-10 pt-10">
+                                        <img src={product?.image} alt="Shoes" className="rounded-xl card-imagee" />
+                                    </figure>
+                                    <div className="card-body  text-center">
+                                    <h2 className="card-title text-start">{product?.toyName}</h2>
+                                        <p className='text-start'>Price: {product?.price}tk</p>
+                                        <div className="card-actions text-start">
+                                            <p>Rating: {product?.ratting}</p> <button className='view-details-btn'>View Detais</button>
+                                        </div>
+                                    </div>
+                                </div></>)
+                            }
+
+                        </div>
                     </div>
                     <div className={toggle === 3 ? "show-content" : "content"}>
-                        <h3>Police</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic necessitatibus similique tempore exercitationem unde nulla nemo assumenda maiores alias neque.</p>
+                        <div className='grid md:grid-cols-2 sm:grid-cols-1  w-[90%] mx-auto '>
+                            {
+                                allProducts?.slice(0 - 2).map(product => <><div className="card flex md:w-96 w-full bg-base-100 shadow-xl my-5 ">
+                                    <figure className="px-10 pt-10">
+                                        <img src={product?.image} alt="Shoes" className="rounded-xl card-imagee" />
+                                    </figure>
+                                    <div className="card-body  text-center">
+                                    <h2 className="card-title text-start">{product?.toyName}</h2>
+                                        <p className='text-start'>Price: {product?.price}tk</p>
+                                        <div className="card-actions text-start">
+                                            <p>Rating: {product?.ratting}</p> <button className='view-details-btn'><Link to={`productDetails/${product?._id}`}>View Detais</Link></button>
+                                        </div>
+                                    </div>
+                                </div></>)
+                            }
+
+                        </div>
+
                     </div>
                 </div>
             </div>
